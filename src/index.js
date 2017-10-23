2cc18b4eb8251f0a702e15e0bda46e372a25b798
Edgar Mueller <emueller@eclipsesource.com>
2017-10-23 12:23:25 +0200

    Add custom renderer files

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import './index.css';
 import schema from './schema.json';
 import uischema from './uischema.json';
 import { initJsonFormsStore } from 'jsonforms-inferno/dist/ts-build/store'
+import { registerRenderer } from 'jsonforms-inferno/dist/ts-build/actions'
+import RatingControl, { ratingControlTester } from "./rating.control"
 
 const store = initJsonFormsStore({
   name: "Max"
 }, schema, uischema);
+
+// Custom control registration
+// store.dispatch(registerRenderer(ratingControlTester, RatingControl));
 
 render(
   <Provider store={store}>
